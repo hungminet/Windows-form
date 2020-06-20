f8f4d024ac486ec8cceea8635a0ddc4fe9411523
hungminet <61225076+hungminet@users.noreply.github.com>
2020-06-20 19:55:01 +0700

    report remaining

---
+++ WindowsFormsApp1/WindowsFormsApp1/DB_layer/DB_Layer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.DB_layer
+{
+    class DB_Layer
+    {
+        string ConnectStr = "Data Source = (local)" +
+        ";Initial Catalog = QUANLIBANHANG;" +
+        "Integrated Security = True";
+        SqlConnection sqlConnection = null;
+        SqlCommand sqlCommand = null;
+        SqlDataAdapter sqlAdapter = null;
+
+        public DB_Layer()
+        {
+            sqlConnection = new SqlConnection(ConnectStr);
+            sqlCommand = sqlConnection.CreateCommand();
+        }
+
+        public DataSet ExeQueryDataSet(string strSQL, CommandType commandtype)
+        {
+            if (sqlConnection.State == ConnectionState.Open)
+                sqlConnection.Close();
+            sqlConnection.Open();
+            sqlCommand.CommandText = strSQL;
+            sqlCommand.CommandType = commandtype;
+            sqlAdapter = new SqlDataAdapter(sqlCommand);
+            DataSet ds = new DataSet();
+            sqlAdapter.Fill(ds);
+            return ds;
+        }
+
+
+
+        public bool ExeNonQuery(string strSQL, CommandType commandType, ref string error)
+        {
+            bool f = false;
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+            sqlConnection.Open();
+            sqlCommand.CommandText = strSQL;
+            sqlCommand.CommandType = commandType;
+            try
+            {
+                sqlCommand.ExecuteNonQuery();
+                f = true;
+            }
+            catch (SqlException ex)
+            {
+                error = ex.Message;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            return f;
+        }
+
+
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Workbase_layer/WB_Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.DB_layer;
+
+namespace WindowsFormsApp1.Workbase_layer
+{
+    class WB_Employee
+    {
+        DB_Layer _dB_Layer = null;
+        public WB_Employee()
+        {
+            _dB_Layer = new DB_Layer();
+        }
+
+        public DataSet GetEmployee()
+        {
+            return _dB_Layer.ExeQueryDataSet("SELECT * FROM NHANVIEN", CommandType.Text);
+        }
+        public bool AddEmployee(string EmID, string em_Name, int sex,
+            int cmnd, int sdt, int tt, ref string err)
+        {
+            string sqlSting = "INSERT INTO NHANVIEN VALUES ("
+                + "'" + EmID + "'"
+                + ",'" + em_Name + "'"
+                + "," + sex
+                + "," + cmnd
+                + ",N'" + sdt + "'"
+                + ",N'" + tt + "'"
+                + ",'" + Hinh + "')";
+            return _dB_Layer.ExeNonQuery(sqlSting, CommandType.Text, ref err);
+        }
+
+        public bool DelEmployee(string Employee_ID, ref string err)
+        {
+            string sqlString = "UPDATE NHANVIEN SET TT_LamViec = 0 + WHERE MaNV = '" + Employee_ID + "'";
+            return _dB_Layer.ExeNonQuery(sqlString, CommandType.Text, ref err);
+        }
+
+        public bool EditEmployee(string Employee_ID, string City_name_edited, ref string err)
+        {
+            string sqlString = "UPDATE ThanhPho SET TenThanhPho =N'" + City_name_edited + "'" + "WHERE ThanhPho = '" + City_ID + "'";
+            return _dB_Layer.ExeNonQuery(sqlString, CommandType.Text, ref err);
+        }
+    }
+}
